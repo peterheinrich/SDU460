ddf56f5fa3649ab7bf43bcb2fd52698a15be150d
peterheinrich <peter.heinrich@its-heinrich.ch>
2023-12-26 10:35:40 +0100

    Minor Cleanup

--- SDU460.js
+++ SDU460.js
@@ -8,81 +8,19 @@ class SDU460 extends HTMLElement {
         fetch("./SDU460.html").then(r => r.text()).then(t => {
             this.innerHTML = t;
         });
-        this.rpm = 1000;
-        this.oildegf = 75;
-        this.oilpsi = 0;
-        this.volts = 0;
-        this.egt = 0;
-        this.cht = 0;
-        this.roll = 0;
-        this.pitch = 0;
-        this.alt = 0;
-        this.airspeed = 0;
-        this.gs = 0;
-        this.splitScreen = false;
-        /*
-                setInterval(() => {
-                    new MessageBus().publish("rpm", this.rpm++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("oil-temperature-degf", this.oildegf++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("oil-pressure-psi", this.oilpsi++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("volts", this.volts += 0.1);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("amps", this.volts += 0.1);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("egt-degf", this.egt++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("cht-degf", this.cht++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("indicated-pitch-deg", Math.sin(this.roll += 0.01) * 20);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("indicated-roll-deg", Math.cos(this.pitch += 0.01) * 30);
-                }, 100);
-        
-                setInterval(() => {
-                    new MessageBus().publish("indicated-altitude-ft", this.alt += 10);
-                }, 100);
-        
-                setInterval(() => {
-                    new MessageBus().publish("indicated-speed-kt", this.airspeed++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("groundspeed-kt", this.gs++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("indicated-heading-deg", this.gs++);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("indicated-track-true-deg", this.gs / 10);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("latitude-deg", 47.078733 + this.gs / 10000);
-                }, 100);
-                setInterval(() => {
-                    new MessageBus().publish("longitude-deg", 9.064843);
-                }, 100);
-        */
     }
 
 
     connectedCallback() {
         document.addEventListener('pushed', this.mainEventHandler);
+        
         var attach = function (ws, path) {
             ws.send(JSON.stringify({
                 command: 'addListener',
                 node: path
             }));
         }
+
         var ws = new WebSocket('ws://localhost:8080/PropertyListener')
         var properties = [
             'instrumentation/altimeter/indicated-altitude-ft',
@@ -110,7 +48,6 @@ class SDU460 extends HTMLElement {
             'consumables/fuel/tank/indicated-level-gal_us',
             'consumables/fuel/tank[1]/indicated-level-gal_us'
         ];
-        console.log(ws);
         ws.onopen = function (ev) {
             properties.forEach(element => {
                 attach(ws, element);
@@ -134,13 +71,11 @@ class SDU460 extends HTMLElement {
             }
         }
         ws.onclose = function (ev) {
-            var msg = 'Lost connection to FlightGear.'
-            console.log(msg);
+            console.log("Lost connection");
         }
 
         ws.onerror = function (ev) {
-            var msg = 'Error communicating with FlightGear.'
-            console.log(msg);
+            console.log("Com Error");
         }
 
         properties.forEach(element => {
@@ -178,12 +113,6 @@ class SDU460 extends HTMLElement {
 
         }
     }
-
-
-    clickEvent() {
-        console.log("Click Click");
-    }
-
 }
 
 customElements.define("g3x-sdu460", SDU460);
