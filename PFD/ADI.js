e53cddd3e772ed5b25d3fcc7a9e6f8faaaf5292b
peterheinrich <peter.heinrich@its-heinrich.ch>
2023-12-26 13:26:13 +0100

    Updating license information

--- PFD/ADI.js
+++ PFD/ADI.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2023, Peter Heinrich <peter@open-simulations.ch>
+ * 
+ * This file is part of OS3X-Touch, a visual look alike of a well
+ * known avionics system for use in desktop flight simulators.
+ * 
+ * OS3X-Touch is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * OS3X-Touch is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
 import { MessageBus } from '../../tools/MessageBus.js';
 
 class ADI extends HTMLElement {
--- SDU460.js
+++ SDU460.js
@@ -1,15 +1,31 @@
+/*
+ * Copyright (C) 2023, Peter Heinrich <peter@open-simulations.ch>
+ * 
+ * This file is part of OS3X-Touch, a visual look alike of a well
+ * known avionics system for use in desktop flight simulators.
+ * 
+ * OS3X-Touch is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * OS3X-Touch is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
 import { MessageBus } from './tools/MessageBus.js';
-
 
 class SDU460 extends HTMLElement {
     constructor() {
         super();
-        this.template = "";
         fetch("./SDU460.html").then(r => r.text()).then(t => {
             this.innerHTML = t;
         });
     }
-
 
     connectedCallback() {
         document.addEventListener('pushed', this.mainEventHandler);
@@ -77,7 +93,8 @@ class SDU460 extends HTMLElement {
         ws.onerror = function (ev) {
             console.log("Com Error");
         }
-
+        
+        /* read all parameters initially from sim */
         properties.forEach(element => {
             fetch("http://localhost:8080/json/" + element).then((response) => {
                 return response.json();
